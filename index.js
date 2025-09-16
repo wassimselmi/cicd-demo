58341b26f7f27a27204e4bf92ff2bd19d55333a3
SELMI Wassim <wassim.selmi@sesame.com.tn>
2025-09-16 06:52:30 +0000

    test workflow

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD demo After modif! 🛠️');
+  res.send('Hello from CI/CD demo After modif now! 🛠️');
 });
 
 app.get('/health', (req, res) => {
