f8f9153b197f098098f2a17f8e2dfbe32d087c39
SELMI Wassim <wassim.selmi@sesame.com.tn>
2025-09-16 06:46:12 +0000

    updated index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD demo! 🛠️');
+  res.send('Hello from CI/CD demo After modif! 🛠️');
 });
 
 app.get('/health', (req, res) => {
