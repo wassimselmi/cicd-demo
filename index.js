ca804cf178f85071a9d310494abbd39408313efa
SELMI Wassim <wassim.selmi@sesame.com.tn>
2025-09-18 02:24:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello! CI CD project under maintenance 🛠️');
+  res.send('Hello! CI CD project under maintenance! 🛠️');
 });
 
 app.get('/health', (req, res) => {
